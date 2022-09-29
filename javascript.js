9e0097058574cc8b3f7b6e5ae0cb38e43798c030
bazooki14 <brandonbouska14@gmail.com>
2022-09-29 13:02:39 -0700

    Create and code playRound function in javascript.js

--- javascript.js
+++ javascript.js
@@ -4,5 +4,29 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 };
 
-const playerSelection = prompt( "Rock, paper, or scissors?").toLowerCase();
-const computerSelection = getComputerChoice();
+let playerSelection;
+let computerSelection;
+
+function playRound(playerSelection, computerSelection) {
+    
+    playerSelection = prompt( "Rock, paper, or scissors?").toLowerCase();
+    computerSelection = getComputerChoice();
+    
+    if (playerSelection === computerSelection) {
+        return "Draw";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors!"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper covers rock!"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors cut paper!"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors cut paper!"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper covers rock!"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors!"
+}
+
+};
+
